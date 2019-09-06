97b43e925336e55fe6bc6f775c4a3bab4b489561
samuel <osamaimafidon@gmail.com>
2019-09-06 01:41:56 +0100

    mvp complete

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,35 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+.then(response => {
+    console.log(response);
+    const topics = response.data.topics;
+    const topicsDiv = document.querySelector(".topics");
+
+    topics.forEach(topic => {
+        Tab = TabCreator(topic);
+        topicsDiv.appendChild(Tab);
+        // Tab.addEventListener('click',handleTabContentDisplay(2,4))
+    })
+
+})
+.catch(err => {
+    console.log(err);
+})
+function handleTabContentDisplay(x,y) {
+    console.log(x+y)
+    return function(e){
+       const attrValue =  e.target.getAttribute('data-tab-topic');
+       const cards = document.querySelector(".cards-container")
+    }
+}
+function TabCreator(topic) {
+    const div = document.createElement('div');
+    div.setAttribute('class','tab');
+    div.textContent = topic;
+    div.setAttribute('data-tab-topic',topic);
+
+    return div;
+}
